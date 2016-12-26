71c813fec5d6de583714376e52c9180df8e6b6a7
pawurb <p.urbanek89@gmail.com>
2016-12-26 21:30:00 +0100

    initial setup

---
+++ TTToolbox/StringExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  StringExtensions.swift
+//  TTToolbox
+//
+//  Created by Pawel Urbanek on 26/12/2016.
+//  Copyright © 2016 Pawel Urbanek. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+  func first() -> Bool {
+    return true
+  }
+}
